82d76590d2951fe2a7ff637d1514a30a41de9cbb
Dawson Timmons <DawsonTimmons@gmail.com>
2023-03-29 17:44:36 -0600

    feat: sends post and delete for one item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,4 +2,31 @@ class Api::V1::ItemsController < ApplicationController
   def index
     render json: ItemSerializer.new(Item.all)
   end
+
+  def show
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+  def create
+    item = Item.new(item_params)
+
+    if item.save
+      render json: ItemSerializer.new(item), status: :created
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+
+  end
+  
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    render status: :no_content
+  end
+
+private
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
+
 end
