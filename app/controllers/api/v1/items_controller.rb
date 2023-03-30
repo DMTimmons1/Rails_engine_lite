c4faf96f098e4ce9c003f45572fecf74f04fc2ca
Dawson Timmons <DawsonTimmons@gmail.com>
2023-03-29 18:11:23 -0600

    feat: send request to update an exiting item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -9,13 +9,22 @@ class Api::V1::ItemsController < ApplicationController
 
   def create
     item = Item.new(item_params)
-
     if item.save
       render json: ItemSerializer.new(item), status: :created
     else
       render json: item.errors, status: :unprocessable_entity
     end
 
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    if item.save
+      render json: ItemSerializer.new(item), status: :created
+    else
+      render json: item.errors, status: :bad_request
+    end
   end
   
   def destroy
