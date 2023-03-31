57ae3e7468505ddbafcfb02679cf2732c8b23f1a
Dawson Timmons <DawsonTimmons@gmail.com>
2023-03-31 02:17:31 -0600

    feat: Add find all method for name and price fragments

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,10 +1,13 @@
 class Api::V1::Items::SearchController < ApplicationController
   def index
-    items = Item.find_all_items_by_name(params[:name])
-    if items != nil
+    if params[:name] != nil && params[:min_price] == nil && params[:max_price] == nil
+      items = Item.find_all_items_by_name(params[:name])
+      render json: ItemSerializer.new(items), status: :ok
+    elsif params[:min_price] != nil && params[:min_price].to_i > 0 && params[:name] == nil && params[:max_price] == nil
+      items = Item.find_all_items_by_price(params[:min_price])
       render json: ItemSerializer.new(items), status: :ok
     else
-      render json: ItemSerializer.new([]), status: :ok
+      render json: ItemSerializer.new([]), status: :bad_request
     end
   end
 end
